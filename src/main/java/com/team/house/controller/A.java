959dab086cbf5cdba5e8a634ea5b5c68584b1d70
solidedit2019 <286479672@qq.com>
2019-07-09 19:57:32 +0800

    111

--- src/main/java/com/team/house/controller/A.java
+++ src/main/java/com/team/house/controller/A.java
@@ -3,5 +3,7 @@ package com.team.house.controller;
 public class A {
     public static void main(String[] args) {
         System.out.println("helloword");
+        System.out.println(111);
+        System.out.println(222);
     }
 }
